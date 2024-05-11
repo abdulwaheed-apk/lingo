b83dac017dab0ba93248a4840e86dc2e03e7f48e
Abdul Waheed Mughal <ab.waheedkashmiri@gmail.com>
2024-05-11 23:59:00 +0300

    auth initial updates

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image'
+import logo from '@/public/duolingo-logo.svg'
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
+import { SignedIn, UserButton } from '@clerk/clerk-react'
 export const Header = () => {
     return (
         <header className='h-20 w-full border-b-2 border-slate-200 px-4'>
-            Header
+            <div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full'>
+                <div className='py-8 pl-4 gap-x-3'>
+                    <Image src={logo} alt='Duolingo logo' height={40} />
+                </div>
+                <ClerkLoading>
+                    <Loader className='h-5 w-5 text-muted-foreground animate-spin' />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </ClerkLoaded>
+            </div>
         </header>
     )
 }
