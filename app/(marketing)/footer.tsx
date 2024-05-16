1ab82cfbe78773b8ffa6d2c30bf8bbc3fc47ee8b
Abdul Waheed Mughal <ab.waheedkashmiri@gmail.com>
2024-05-17 00:38:53 +0300

    footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,21 +1,45 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import es from '@/public/es.svg'
+import fr from '@/public/fr.svg'
+import de from '@/public/de.svg'
+import tr from '@/public/tr.svg'
+import hr from '@/public/hr.svg'
+import pt from '@/public/pt.svg'
+import hindi from '@/public/in.svg'
+import it from '@/public/it.svg'
+
+const flags = [
+    { country: 'Spanish', src: es },
+    { country: 'French', src: fr },
+    { country: 'German', src: de },
+    { country: 'Turkish', src: tr },
+    { country: 'Italian ', src: it },
+    { country: 'Portuguese', src: pt },
+    { country: 'Hindi', src: hindi },
+]
 
 export const Footer = () => {
     return (
         <footer className='hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2'>
             <div className='max-w-screen-lg mx-auto flex items-center justify-evenly h-full'>
-                <Button variant={'ghost'} size={'lg'} className='w-full'>
-                    <Image
-                        alt='Spanish'
-                        src={es}
-                        width={40}
-                        height={32}
-                        className='rounded-md mr-4'
-                    />
-                    Spanish
-                </Button>
+                {flags.map((flag, index) => (
+                    <Button
+                        key={index}
+                        variant={'ghost'}
+                        size={'lg'}
+                        className='w-full'
+                    >
+                        <Image
+                            alt={flag.country}
+                            src={flag.src}
+                            width={40}
+                            height={32}
+                            className='rounded-md mr-4'
+                        />
+                        {flag.country}
+                    </Button>
+                ))}
             </div>
         </footer>
     )
